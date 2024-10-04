20b828b4ae2858bdca36f3f5a5480d50b297647f
WilliamAnimate <nyameowmeow@duck.com>
2024-10-04 19:25:27 -0400

    feat: crude configuration support

---
+++ src/configuration.rs
@@ -0,0 +1,35 @@
+use serde::Deserialize;
+
+#[cfg(target_os = "linux")]
+static DEFAULT_CFG_PATH: &'static str = ".config/echotune/echotune.toml";
+
+#[derive(Deserialize, Debug)]
+pub struct Config {
+    pub main: TomlMain
+}
+
+#[derive(Deserialize, Debug)]
+pub struct TomlMain {
+    pub crash_on_execute: bool,
+}
+
+impl Config {
+    pub fn parse(to_parse: echotune::ConfigurationMode) -> Self {
+        use std::fs::read_to_string;
+
+        let file = match to_parse {
+            echotune::ConfigurationMode::Default => DEFAULT_CFG_PATH,
+            echotune::ConfigurationMode::Custom(s) => s
+        };
+        #[allow(deprecated)]
+        let file = format!("{}/{}", std::env::home_dir().unwrap().to_string_lossy().to_string(), file);
+
+        let buf = read_to_string(file).unwrap();
+
+        let parsed: Config = basic_toml::from_str(&buf).unwrap();
+        dbg!(&parsed);
+
+        parsed
+    }
+}
+
