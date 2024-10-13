5f6154df2b48dde58c46338153e986b258800604
WilliamAnimate <nyameowmeow@duck.com>
2024-10-12 20:44:51 -0400

    revert: "fix: check if file can be read before putting in playlist"
    This reverts commit 55d2d5c108ffa5f74f035fa8bbf3cb66c28dac3d.
    
    it doesnt work

--- src/main.rs
+++ src/main.rs
@@ -52,8 +52,8 @@ fn parse_playlist(file: &str) -> Result<(), Box<dyn std::error::Error>> {
             continue; // its a comment; skip
         }
         line = line.replacen('~', &home, 1);
-        if File::open(&line)?.metadata()?.permissions().readonly() {
-            lines.push(line);
+        if File::open(&line).is_ok() {
+            lines.push(line); // file exists, therefore, push it onto the playlist
         }
     }
     lines.shrink_to_fit();
